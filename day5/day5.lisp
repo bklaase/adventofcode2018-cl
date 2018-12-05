a6ec570281171583eaa3c212a31256ef9029f933
Bob <psilioxus@haplo.localdomain>
2018-12-05 22:30:53 +0100

    completed day5

--- day5/day5.lisp
+++ day5/day5.lisp
@@ -2,6 +2,7 @@
 
 ;; load deps
 (ql:quickload :cl-ppcre)
+(ql:quickload :cl-interpol)
 
 ;; package definition
 (defpackage :aoc2018/day5
@@ -10,6 +11,7 @@
 ;;; package
 (in-package :aoc2018/day5)
 
+(named-readtables:in-readtable :interpol-syntax)
 
 ;; puzzle input
 (defvar *input*)
@@ -21,17 +23,38 @@
   (let (( a (char-code char-a)) (b (char-code char-b)))
 	(= 32 (- (max a b) (min a b)))))
 
+(defun react-polymer (string)
+  "recursively removes all pairs uU and Uu and returns striped 'polymer'"
+  (reduce
+   (lambda (x xs)
+	 (if
+	  (or (null xs) (not (chars-same-different-case (car xs) x)))
+	  (cons x xs)
+	  (rest xs)))
+   string :from-end t :initial-value nil))
 
+(defun strip-char-from-string (char string)
+  "stips all (upper and lowercaser) versions of char from string"
+  (let* ((d (char-downcase char)) (u (char-upcase char))
+		 (expr #?"[${u}${d}]"))
+	(cl-ppcre:regex-replace-all expr string "")))
 
 ;;; solutions
-;;3a
-(defun 5a (string) 
-  (let ((ptrn (create-scanner "([A-Z])(?!\1)([a-z])|([a-z])(?!\3)([A-Z])")))
-	(labels ((5a-rec (string previouslength)
-			   (break)
-			   (let* ((new-string (cl-ppcre:regex-replace-all ptrn string ""))
-					  (new-length (length new-string)))
-				 (if (= new-length previouslength)
-					 new-string
-					 (5a-rec new-string new-length)))))
-	(5a-rec string (length string)))))
+;;5a
+(defun 5a (string)
+  (length (react-polymer string)))
+
+;;5b
+(defun 5b (string)
+  (let ((max-impacting-char) (max-impact (length string)))
+	(dotimes (i 26)
+	  (let* ((c (code-char (+ i 97)))
+			 (impact (length (react-polymer (strip-char-from-string c string)))))
+		(when (< impact max-impact)
+		  (setf max-impact impact
+				max-impacting-char c))))
+	max-impact))
+
+(defun print-answers ()
+  (format t "5a: polymer-length: ~a~%5b: shortest polymer: ~a"
+		  (5a *input*) (5b *input*)))
